fb164d22537bd968a7b3f1691a60d5d343691924
leiperjack <leiperjack1997@gmail.com>
2019-03-04 08:51:22 +0000

    update

--- visits/index.js
+++ visits/index.js
@@ -6,14 +6,16 @@ const client = redis.createClient({
     host: 'redis-server',
     port: 6379
 });
+client.set('visits', 0);
 
 app.get('/', (req, res) => {
-    client.get('visits', (err,visits) => {
+    process.exit(0);
+    client.get('visits', (err, visits) => {
         res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
     });
 });
 
 app.listen(8081, () => {
-    console.log('Listening on port 8081');
+    console.log('Listening on port 4001');
 });
